ab6997d9028c41177b18467289a0c8eea0124f6c
Образцов Григорий Владимирович <Grigoriy.Obraztsov@rosbank.ru>
2023-04-03 22:51:57 +0300

    Initial commit

---
+++ src/main/java/ru/grobikon/oauth2/backend/controller/AdminController.java
@@ -0,0 +1,19 @@
+package ru.grobikon.oauth2.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin") // базовый URI
+public class AdminController {
+
+    // добавление
+    @GetMapping("/data")
+    public String admin() {
+        return "admin data";
+    }
+
+
+
+}
